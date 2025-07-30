73cbf1ca63c75fe96828eb5de8f302d27de58682
Kamila Santos <kamilasantosdev@gmail.com>
2025-07-30 17:11:09 -0300

    [0002-02] refactor: update RabbitMQ config to support multiple dynamic queues instead of single queue

--- consumer/src/main/java/com/ppm/delivery/order/consumer/config/RabbitConfig.java
+++ consumer/src/main/java/com/ppm/delivery/order/consumer/config/RabbitConfig.java
@@ -1,42 +1,89 @@
 package com.ppm.delivery.order.consumer.config;
 
-import org.springframework.amqp.core.*;
+import com.ppm.delivery.order.consumer.properties.MessageProperties;
+import com.ppm.delivery.order.consumer.properties.SupportedCountries;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableRabbit
 public class RabbitConfig {
 
-    @Value("${app.rabbitmq.domain}")
-    private String domain;
+    private final SupportedCountries supportedCountries;
+    private final MessageProperties messageProperties;
 
-    @Value("${app.rabbitmq.country}")
-    private String country;
-
-    @Value("${app.rabbitmq.action-create}")
-    private String actionCreate;
-
-    @Value("${app.rabbitmq.exchange-command}")
-    private String commandExchangeName;
+    public RabbitConfig(SupportedCountries supportedCountries, MessageProperties messageProperties){
+        this.supportedCountries = supportedCountries;
+        this.messageProperties = messageProperties;
+    }
 
     @Bean
     public TopicExchange commandExchange() {
-        return new TopicExchange(commandExchangeName);
+        return new TopicExchange(messageProperties.getExchange());
     }
 
     @Bean
-    public Queue createQueue() {
-        String queueName = country + "." + domain + "." + actionCreate;
-        return new Queue(queueName, true);  // durable = true
+    public List<Queue> countryQueues() {
+
+        List<Queue> countryQueues = new ArrayList<>();
+        String domain = messageProperties.getDomain();
+        for(String country : supportedCountries.getSupportedCountries()) {
+            for (String action :  messageProperties.getActions()) {
+                countryQueues.add(new Queue(country.toLowerCase() + "." + domain + "." + action, true));
+            }
+        }
+        return countryQueues;
     }
 
     @Bean
-    public Binding bindingCreateQueue(Queue createQueue, TopicExchange commandExchange) {
-        String routingKey = country + "." + actionCreate;
-        return BindingBuilder.bind(createQueue).to(commandExchange).with(routingKey);
+    public List<String> queueNames() {
+        return countryQueues().stream()
+                .map(Queue::getName)
+                .toList();
+    }
+
+    @Bean
+    public List<Binding> createBindings(TopicExchange commandExchange, List<Queue> countryQueues) {
+        List<Binding> bindings = new ArrayList<>();
+        for (Queue queue : countryQueues) {
+            String country = queue.getName().split("\\.")[0];
+
+            for (String action :  messageProperties.getActions()) {
+                String routingKey = country + "." + action;
+                bindings.add(BindingBuilder.bind(queue).to(commandExchange).with(routingKey));
+            }
+        }
+        return bindings;
+    }
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory,
+                                   TopicExchange commandExchange,
+                                   List<Queue> countryQueues,
+                                   List<Binding> createBindings) {
+
+        RabbitAdmin admin = new RabbitAdmin(connectionFactory);
+        admin.declareExchange(commandExchange);
+
+        for (Queue queue : countryQueues) {
+            admin.declareQueue(queue);
+        }
+
+        for (Binding binding : createBindings) {
+            admin.declareBinding(binding);
+        }
+
+        return admin;
     }
 
 }
