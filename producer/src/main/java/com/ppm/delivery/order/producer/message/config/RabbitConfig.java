5aa25c02f12b0f1ab724ed777bbb6ab8f132d5a2
Kamila Santos <kamilasantosdev@gmail.com>
2025-08-21 15:07:15 -0300

    [0002-15] fix: enable Jackson2JsonMessageConverter in RabbitConfig for proper message serialization

--- producer/src/main/java/com/ppm/delivery/order/producer/message/config/RabbitConfig.java
+++ producer/src/main/java/com/ppm/delivery/order/producer/message/config/RabbitConfig.java
@@ -12,15 +12,15 @@ import org.springframework.context.annotation.Configuration;
 @EnableRabbit
 public class RabbitConfig {
 
-//    @Bean
-//    public MessageConverter messageConverter() {
-//        return new Jackson2JsonMessageConverter();
-//    }
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
 
     @Bean
     public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
         RabbitTemplate template = new RabbitTemplate(connectionFactory);
-//        template.setMessageConverter(messageConverter);
+        template.setMessageConverter(messageConverter);
         return template;
     }
 }
